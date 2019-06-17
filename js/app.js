ebdc0ad0bcadb752198589fed9c3ca3ed0512216
Kenny Seo <44145254+seokenny@users.noreply.github.com>
2019-06-16 22:08:19 -0400

    Mobile nav / transition + mobile content layout

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// $(".work_descript_text").on('click',function(){
+//     $(".work_descript_text").animate({opacity:0});
+// });
+
+// $(".work_descript_header").on('click',function(){
+//     $(".work_descript_text").animate({opacity:1});
+// });
+
+$(".work_circle").on('click',function(){
+    $(".rugsusa_work_page").fadeIn();
+});
+
+$(".work_page_banner").on('click',function(){
+    $(".rugsusa_work_page").fadeOut();
+});
